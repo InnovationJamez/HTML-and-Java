44a97a6a96db02d6160d728dcf2856bde96d8f7f
WIT\lehoej <s3734925@student.rmit.edu.au>
2019-03-09 13:12:38 -0500

    more

---
+++ Showcase/javascript/Canvas_Object.js
@@ -0,0 +1,201 @@
+function Canvas_Object(canvasId) {
+	this.canvasId = document.querySelector('canvas');
+	this.canvasId.width = window.innerWidth * 0.99;
+	this.canvasId.height = window.innerHeight * 0.99;
+	this.context = this.canvasId.getContext("2d");
+
+	// for storing the charges
+	this.chargeList = [];
+	this.numCharges = 0;
+
+	// for storing the arrows
+	this.arrowList = [];
+	this.numArrows = 0;
+
+	// for creating the arrow field
+	this.rowNum = Math.floor(this.canvasId.height / 25);
+	this.columnNum = Math.floor(this.canvasId.width / 25);
+
+	// for storing the field probes
+	this.probeList = [];
+	this.numbProbes = 0;
+
+	// for buttons
+	this.buttonList = [];
+
+	// create arrows
+	this.createArrows = function() {
+		var arrowX, arrowY;
+		for(var i = 0; i < this.columnNum * this.rowNum; i++){
+		arrowX = ((i + this.columnNum) % this.columnNum) * this.canvasId.width / 
+			this.columnNum + this.canvasId.width / (2 * this.columnNum);
+		arrowY = Math.floor(i / this.columnNum) * this.canvasId.height / 
+			this.rowNum + this.canvasId.height / (2 * this.rowNum);
+		this.arrowList.push(new Field_Arrow(arrowX, arrowY, this.canvasId.height));
+		}		
+	}
+
+	// add on arrow to specified location
+
+	this.addArrow = function(x, y) {
+		this.arrowList.push(new Field_Arrow(x, y, this.canvasId.height));
+	}
+
+	// update arrows
+
+	this.updateArrows = function() {
+		this.numArrows = this.arrowList.length;
+
+		for(var i = 0; i < this.numArrows; i++) {
+			this.arrowList[i].update(this.chargeList);
+			this.arrowList[i].draw(this.context);
+		}
+	}
+
+
+	// create charge
+
+	this.addCharge = function(x, y, q) {
+		this.chargeList.push(new Charge(x, y, q, this.canvasId.height));
+	}
+
+	// remove charge
+
+	this.removeCharge = function(){
+		this.chargeList.pop();
+	}
+
+	// update charges
+
+	this.updateCharges = function() {
+		this.numcharges = this.chargeList.length;
+
+		for(var i = 0; i < this.numcharges; i++){
+			this.chargeList[i].update();
+			this.chargeList[i].draw(this.context);
+		}
+	}
+
+
+	// create probe
+
+	this.addProbe = function(x, y) {
+		this.probeList.push(new Field_Probe(x, y, this.canvasId.height));
+	}
+
+	// update probe
+
+	this.updateProbes = function() {
+		this.numbProbes = this.probeList.length;
+
+		for(var i = 0; i < this.numbProbes; i++) {
+			this.probeList[i].update(this.chargeList);
+			this.probeList[i].draw(this.context);
+		}
+
+	}
+
+	// create button
+	this.addButton = function(x1, y1, x2, y2, qType, angle){
+		this.buttonList.push(new Text_Box(x1, y1, x2, y2, qType, angle));
+	}
+
+	// create buttons
+	this.createButtons = function() {
+		// question bar : 0
+		this.addButton(this.canvasId.width * 0.05, this.canvasId.height * 0.03, 
+			this.canvasId.width * 0.95, this.canvasId.height * 0.1, 0, 0);
+
+		// submitt : 1
+		this.addButton(this.canvasId.width * 0.01, this.canvasId.height * 0.95, 
+			this.canvasId.width * 0.33, this.canvasId.height * 0.999, 2);
+
+		// show field : 2
+		this.addButton(this.canvasId.width * 0.33, this.canvasId.height * 0.95, 
+			this.canvasId.width * 0.66, this.canvasId.height * 0.999, 3);
+
+		// main menue : 3
+		this.addButton(this.canvasId.width * 0.66, this.canvasId.height * 0.95, 
+			this.canvasId.width * 0.99, this.canvasId.height * 0.999, 4);
+
+		// answer questions : 4
+		this.addButton(this.canvasId.width * 0.20, this.canvasId.height * 0.40, 
+			this.canvasId.width * 0.80, this.canvasId.height * 0.50, 5);
+
+		// freeplay : 5
+		this.addButton(this.canvasId.width * 0.20, this.canvasId.height * 0.60, 
+			this.canvasId.width * 0.80, this.canvasId.height * 0.70, 6);
+
+		// add charge : 6
+		this.addButton(this.canvasId.width * 0.01, this.canvasId.height * 0.95, 
+			this.canvasId.width * 0.33, this.canvasId.height * 0.999, 7);
+
+		// remove charge : 7
+		this.addButton(this.canvasId.width * 0.33, this.canvasId.height * 0.95, 
+			this.canvasId.width * 0.66, this.canvasId.height * 0.999, 8);
+
+		// correct : 8
+		this.addButton(this.canvasId.width * 0.20, this.canvasId.height * 0.10, 
+			this.canvasId.width * 0.80, this.canvasId.height * 0.20, 9);
+
+		//incorrect : 9
+		this.addButton(this.canvasId.width * 0.20, this.canvasId.height * 0.10, 
+			this.canvasId.width * 0.80, this.canvasId.height * 0.20, 10);
+	}
+
+	// reset canvas
+
+	this.resetCanvas = function(){
+		this.chargeList = [];
+		this.probeList = [];
+
+		var buttonListLength = this.buttonList.length;
+
+		for(var i = 0; i < buttonListLength; i++){
+			this.buttonList[i].drawn = false;
+		}
+	}
+
+	// update the canvas
+
+	this.drawQuestionBox = function(value) {
+		switch(value){
+			case 0: // main menue
+				this.buttonList[4].draw(this.context);
+				this.buttonList[5].draw(this.context);
+				break;
+			case 1: // free play
+				this.buttonList[6].draw(this.context);
+				this.buttonList[7].draw(this.context);
+				this.buttonList[3].draw(this.context);
+				break;
+			case 2: // question mode
+				this.buttonList[0].draw(this.context);
+				this.buttonList[1].draw(this.context);
+				this.buttonList[2].draw(this.context);
+				this.buttonList[3].draw(this.context);
+				break;
+		}
+	}
+
+	this.updateCanvas = function() {
+		this.context.clearRect(0,0,innerWidth, innerHeight);
+
+		this.updateArrows();
+
+		this.updateCharges(); 
+
+		this.updateProbes();
+	}
+}
+
+/*
+var canOne = new Canvas_Object("canvasOne");
+canOne.createArrows();
+canOne.addCharge(100, 200, 2);
+canOne.addCharge(100, 400, 2);
+canOne.addProbe(100, 300);
+canOne.updateCanvas();
+canOne.createButtons();
+canOne.drawQuestionBox(1);
+*/
---
+++ Showcase/javascript/revisedQuestionair.js
@@ -0,0 +1,88 @@
+/*
+	1: decimal to binary 
+	2: binary to decimal
+	3: binary to octal
+	4: octal to binary
+	5: binary to hex
+	6: hex to binary
+*/
+
+function Question(questionId, answerId, questionType){
+	this.questionId = questionId;
+	this.answerId = answerId;
+	this.questionType = questionType;
+	this.decNum = Math.floor(Math.random()*50);
+	this.binNum = this.decNum.toString(2);
+	this.octNum = this.decNum.toString(8);
+	this.hexNum = this.decNum.toString(16);
+
+	switch(this.questionType){
+		case 1:
+			first = "<b>decimal</b>: <b>" + this.decNum + "</b>";
+			second = "<b>binary</b>";
+			this.correctAnswer = this.binNum;
+			break;
+		case 2:
+			first = "<b>binary</b>: <b>" + this.binNum + "</b>";
+			second = "<b>decimal</b>";
+			this.correctAnswer = this.decNum;
+			break;
+		case 3:
+			first = "<b>binary</b>: <b>" + this.binNum + "</b>";
+			second = "<b>octal</b>";
+			this.correctAnswer = this.octNum;
+			break;
+		case 4:
+			first = "<b>octal</b>: <b>" + this.octNum + "</b>";
+			second = "<b>binary</b>";
+			this.correctAnswer = this.binNum;
+			break;
+		case 5:
+			first = "<b>binary</b>: <b>" + this.binNum + "</b>";
+			second = "<b>hex</b>";
+			this.correctAnswer = this.hexNum;
+			break;
+		case 6:
+			first = "<b>hex</b>: <b>" + this.hexNum + "</b>";
+			second = "<b>binary</b>";
+			this.correctAnswer = this.binNum;
+			break;	
+	}
+	document.getElementById(this.questionId).innerHTML 
+	= ("Convert from " + first + " to equivalent " + second + " value!");
+}
+
+q1 = new Question("q1","a1", 1);
+q2 = new Question("q2","a2", 2);
+q3 = new Question("q3","a3", 3);
+q4 = new Question("q4","a4", 4);
+q5 = new Question("q5","a5", 5);
+q6 = new Question("q6","a6", 6);
+
+questionList = [q1, q2, q3, q4, q5, q6];
+
+function checkAnswers(){
+	var score = 0;
+	var total = 0;
+	questionList.forEach(function tallyResults(element){
+		if(document.getElementById(element.answerId).value == element.correctAnswer){
+			document.getElementById(element.answerId).style.backgroundColor = "green";
+			score++;
+			total++;
+		}
+		else{
+			document.getElementById(element.answerId).style.backgroundColor = "red";
+			total++;
+		}
+	});
+	document.getElementById("score").innerHTML = score + "/" + total; 
+}
+
+/*
+
+ note: in conversions to binary the program cannot differentiate 001 and 1
+ and leading zero's need to be removed for the program to recegnise the answer
+
+ */
+
+
